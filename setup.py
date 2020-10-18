1a65231327da681eb2aaa982a0c7ab3f521b989d
Craig <cdmacfdayen@gmail.com>
2020-10-18 11:38:29 +0100

    Create setup.py for release to pypi

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+
+    name="emailbot",
+    version="0.0.0",
+    author="Craig Macfadyen",
+    author_email="cdmacfadyen@gmail.com",
+    description="Simple package to email you when scripts are finished.",
+    url="https://github.com/cdmacfadyen/emailbot",
+    packages=["emailbot"],
+    keywords=["email","simple"],
+    classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',      
+    ],
+)
