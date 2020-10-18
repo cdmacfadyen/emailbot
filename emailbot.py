050a90cc9fa182367f774110411915c01320f68f
Craig <cdmacfdayen@gmail.com>
2020-10-18 11:23:57 +0100

    Change to object oriented

--- emailbot.py
+++ emailbot.py
@@ -4,31 +4,42 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
 import smtplib, ssl, email
-EMAIL = "EMAIL"
 
-def get_password(identifier):
-  with open(os.path.expanduser("SECRETS FILE")) as f:
-    for line in f:
-      if identifier in line:
-        tokens = line.split("\t")
-        tokens = [token.rstrip() for token in tokens]
-        return tokens
-    
 
-def email_me(message="Task Complete"):
-    port = 465
+class EmailBot:
+  email_address = ""
+  account_name = ""
+  
+  def __init__(self, recipient, bot_identifier):
+    self.email_address = recipient
+    self.bot_identifier = bot_identifier
 
-    username, password = get_password("BOT NAME")
-    context = ssl.create_default_context()
+  def get_password(self):
+    with open(os.path.expanduser("~/.emailbot")) as f:
+      for line in f:
+        if self.bot_identifier in line:
+          tokens = line.split("\t")
+          tokens = [token.rstrip() for token in tokens]
+          print(tokens)
+          print(tokens[1:])
+          return tokens[1:]
+  
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        msg = MIMEMultipart()
-        msg["From"] = username
-        msg["To"] = EMAIL
-        msg["Subject"] = "Task Complete"
+      
 
-        body = message
-        msg.attach(MIMEText(body, "plain"))
-        server.login(username, password)
-        server.sendmail(username, EMAIL, msg.as_string() )
+  def email_me(self, message="Task Complete", subject="Task Complete"):
+      port = 465
 
+      username, password = self.get_password()
+      context = ssl.create_default_context()
+
+      with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+          msg = MIMEMultipart()
+          msg["From"] = username
+          msg["To"] = self.email_address
+          msg["Subject"] = subject
+
+          body = message
+          msg.attach(MIMEText(body, "plain"))
+          server.login(username, password)
+          server.sendmail(username, self.email_address, msg.as_string() )
