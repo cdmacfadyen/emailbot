88239c980f23a1b9c4032a47c1323fc404aaaf43
Craig <cdmacfdayen@gmail.com>
2020-10-18 11:59:07 +0100

    changed order of arguemts to be more intuitive

--- emailbot/emailbot.py
+++ emailbot/emailbot.py
@@ -9,13 +9,18 @@ import smtplib, ssl, email
 class EmailBot:
   email_address = ""
   account_name = ""
-  
+  secrets_path = "~/.emailbot"
+
   def __init__(self, recipient, bot_identifier):
     self.email_address = recipient
     self.bot_identifier = bot_identifier
 
+  
+  def set_secrets_path(self, path):
+    self.secrets_path = path
+  
   def get_password(self):
-    with open(os.path.expanduser("~/.emailbot")) as f:
+    with open(os.path.expanduser(self.secrets_path)) as f:
       for line in f:
         if self.bot_identifier in line:
           tokens = line.split("\t")
@@ -24,7 +29,7 @@ class EmailBot:
           print(tokens[1:])
           return tokens[1:]
 
-  def email_me(self, message="Task Complete", subject="Task Complete"):
+  def email_me(self, subject="Task Complete", message="Task Complete"):
       port = 465
 
       username, password = self.get_password()
